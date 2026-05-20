5baa2725119d02e576c3a599c7893f0d1de5335c
Lennouille <lennygadroy@gmail.com>
2026-05-20 20:39:05 +0200

    add reals to section

--- js/work.js
+++ js/work.js
@@ -4,65 +4,65 @@ const WorkHTML = `
         <div class="Container">
             <div class="Card">
                 <div class="CardImg">
-                    <img src="/img/" alt="">
+                    <img src="/img/reals/mu/pc-home.webp" alt="Aperçu de la page d'accueil de Mü">
                 </div>
                 <div class="CardDesc">
-                    <p>Lorem</p>
-                    <p>Lorem</p>
+                    <p>Mü être</p>
+                    <p>6C3E86</p>
                 </div>
             </div>
             <div class="Card">
                 <div class="CardImg">
-                    <img src="/img/" alt="">
+                    <img src="/img/reals/portfolio/pc-home.webp" alt="Aperçu de la page d'accueil du portfolio de Lenny Gadroy">
                 </div>
                 <div class="CardDesc">
-                    <p>Lorem</p>
-                    <p>Lorem</p>
+                    <p>Portfolio personnel</p>
+                    <p>447794</p>
                 </div>
             </div>
             <div class="Card">
                 <div class="CardImg">
-                    <img src="/img/" alt="">
+                    <img src="/img/reals/primaire/pc-home.webp" alt="Aperçu de la page d'accueil de Primaire">
                 </div>
                 <div class="CardDesc">
-                    <p>Lorem</p>
-                    <p>Lorem</p>
+                    <p>Primaire</p>
+                    <p>D8820F</p>
                 </div>
             </div>
             <div class="Card">
                 <div class="CardImg">
-                    <img src="/img/" alt="">
+                    <img src="/img/reals/goon/pc-home.webp" alt="Aperçu de la page d'accueil de Goon">
                 </div>
                 <div class="CardDesc">
-                    <p>Lorem</p>
-                    <p>Lorem</p>
+                    <p>Goon</p>
+                    <p>35577D</p>
                 </div>
             </div>
             <div class="Card">
                 <div class="CardImg">
-                    <img src="/img/" alt="">
+                    <img src="/img/reals/geoptime/pc-home.webp" alt="Aperçu de la page d'accueil de Geoptime">
                 </div>
                 <div class="CardDesc">
-                    <p>Lorem</p>
-                    <p>Lorem</p>
+                    <p>GeopTime</p>
+                    <p>153352</p>
                 </div>
             </div>
             <div class="Card">
                 <div class="CardImg">
-                    <img src="/img/" alt="">
+                    <img src="/img/reals/HabitsTracker/pc-home.webp" alt="Aperçu de la page d'accueil de HabitsTracker">
                 </div>
                 <div class="CardDesc">
-                    <p>Lorem</p>
-                    <p>Lorem</p>
+                    <p>Habits Tracker</p>
+                    <p>080A12</p>
                 </div>
             </div>
             <div class="Card">
                 <div class="CardImg">
-                    <img src="/img/" alt="">
+                    <img src="/img/reals/GymAddict/pc-home.webp" alt="Aperçu de la page d'accueil de GymAddict">
                 </div>
                 <div class="CardDesc">
-                    <p>Lorem</p>
-                    <p>Lorem</p>
+                    <p>GymAddict</p>
+                    <p>22D97A</p>
                 </div>
             </div>
         </div>
