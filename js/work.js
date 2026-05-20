6da1d570adcc003fa60284cf0e119642ca629a68
Lennouille <lennygadroy@gmail.com>
2026-05-20 20:12:54 +0200

    create work section

---
+++ js/work.js
@@ -0,0 +1,72 @@
+const WorkHTML = `
+    <section class="Work">
+        <div class="Letters"></div>
+        <div class="Container">
+            <div class="Card">
+                <div class="CardImg">
+                    <img src="/img/" alt="">
+                </div>
+                <div class="CardDesc">
+                    <p>Lorem</p>
+                    <p>Lorem</p>
+                </div>
+            </div>
+            <div class="Card">
+                <div class="CardImg">
+                    <img src="/img/" alt="">
+                </div>
+                <div class="CardDesc">
+                    <p>Lorem</p>
+                    <p>Lorem</p>
+                </div>
+            </div>
+            <div class="Card">
+                <div class="CardImg">
+                    <img src="/img/" alt="">
+                </div>
+                <div class="CardDesc">
+                    <p>Lorem</p>
+                    <p>Lorem</p>
+                </div>
+            </div>
+            <div class="Card">
+                <div class="CardImg">
+                    <img src="/img/" alt="">
+                </div>
+                <div class="CardDesc">
+                    <p>Lorem</p>
+                    <p>Lorem</p>
+                </div>
+            </div>
+            <div class="Card">
+                <div class="CardImg">
+                    <img src="/img/" alt="">
+                </div>
+                <div class="CardDesc">
+                    <p>Lorem</p>
+                    <p>Lorem</p>
+                </div>
+            </div>
+            <div class="Card">
+                <div class="CardImg">
+                    <img src="/img/" alt="">
+                </div>
+                <div class="CardDesc">
+                    <p>Lorem</p>
+                    <p>Lorem</p>
+                </div>
+            </div>
+            <div class="Card">
+                <div class="CardImg">
+                    <img src="/img/" alt="">
+                </div>
+                <div class="CardDesc">
+                    <p>Lorem</p>
+                    <p>Lorem</p>
+                </div>
+            </div>
+        </div>
+    </section>
+`;
+
+document.getElementById('WorkPlaceHolder').innerHTML = WorkHTML;
