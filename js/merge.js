bfc3a5433e72306a07a6154ba0df62431d60ccdf
Lennouille <lennygadroy@gmail.com>
2026-05-21 12:31:09 +0200

    change color : only primary

--- js/merge.js
+++ js/merge.js
@@ -3,26 +3,26 @@ const mergeHTML = `
     <div class="ListContainer">
       <p class="ListText">Ma méthode :</p>
       <ul class="List">
-        <li style="--i: 0">auditer.</li>
-        <li style="--i: 1">observer.</li>
-        <li style="--i: 2">comprendre.</li>
-        <li style="--i: 3">esquisser.</li>
-        <li style="--i: 4">structurer.</li>
-        <li style="--i: 5">concevoir.</li>
-        <li style="--i: 6">maquetter.</li>
-        <li style="--i: 7">harmoniser.</li>
-        <li style="--i: 8">sublimer.</li>
-        <li style="--i: 9">prototyper.</li>
-        <li style="--i: 10">animer.</li>
-        <li style="--i: 11">tester.</li>
-        <li style="--i: 12">itérer.</li>
-        <li style="--i: 13">simplifier.</li>
-        <li style="--i: 14">adapter.</li>
-        <li style="--i: 15">accessibiliser.</li>
-        <li style="--i: 16">optimiser.</li>
-        <li style="--i: 17">intégrer.</li>
-        <li style="--i: 18">livrer.</li>
-        <li style="--i: 19">recommencer.</li>
+        <li>auditer.</li>
+        <li>observer.</li>
+        <li>comprendre.</li>
+        <li>esquisser.</li>
+        <li>structurer.</li>
+        <li>concevoir.</li>
+        <li>maquetter.</li>
+        <li>harmoniser.</li>
+        <li>sublimer.</li>
+        <li>prototyper.</li>
+        <li>animer.</li>
+        <li>tester.</li>
+        <li>itérer.</li>
+        <li>simplifier.</li>
+        <li>adapter.</li>
+        <li>accessibiliser.</li>
+        <li>optimiser.</li>
+        <li>intégrer.</li>
+        <li>livrer.</li>
+        <li>recommencer.</li>
       </ul>
     </div>
   </section>
