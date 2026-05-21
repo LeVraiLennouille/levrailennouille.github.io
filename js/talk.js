c347352982f61205fb36ef89982f84b55e9a57e9
Lennouille <lennygadroy@gmail.com>
2026-05-21 16:03:27 +0200

    upgrade TalkBadge UI + text

--- js/talk.js
+++ js/talk.js
@@ -1,7 +1,7 @@
 const TalkHTML = `
     <section class="Talk" aria-label="Prendre contact">
         <div class="TalkContainer">
-            <span class="TalkBadge">Disponible pour de nouvelles opportunités</span>
+            <span class="TalkBadge">Disponible pour de nouveaux projets</span>
             <h2 class="TalkTitle">Un projet en tête ?<br>Parlons-en.</h2>
             <p class="TalkSub">Que ce soit pour votre site internet, votre boutique ou votre portfolio, je suis à votre écoute et prêt à donner vie à vos idées ! Partant ?</p>
             <div class="NextWrapper">
