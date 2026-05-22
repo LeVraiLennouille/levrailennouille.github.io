9f6619033cc99205b4cf154bfcb6e82e2a719782
Lennouille <lennygadroy@gmail.com>
2026-05-22 11:29:27 +0200

    change (alt)text

--- js/header.js
+++ js/header.js
@@ -1,14 +1,14 @@
 const headerHTML = `
     <header class="MainHeader">
         <nav class="NavBar">
-            <a class="NavLogo" href="/index" aria-label="Retour à l'accueil"><img src="/assets/icon.svg" alt="Logo de Lenny Gadroy" /></a>
+            <a class="NavLogo" href="/index" aria-label="Retour à l'accueil"><img src="/assets/icon.svg" alt="Logo de Lennouille" /></a>
             <ul class="NavLinks">
                 <li><a href="/index">Accueil</a></li>
                 <li><a href="/services">Services</a></li>
                 <li><a href="/reals">Réalisations</a></li>
                 <li><a href="/about">A propos</a></li>
             </ul>
-            <div class="NavBtn"><a href="/contact" class="BtnSolid">Me contacter</a></div>
+            <div class="NavBtn"><a href="/contact" class="BtnSolid">Contactez moi</a></div>
         </nav>
     </header>
 `;
