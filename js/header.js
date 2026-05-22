efec6425a7223f1375ce2e4ce9260ae7bc059d70
Lennouille <lennygadroy@gmail.com>
2026-05-22 11:28:57 +0200

    add new links

--- js/header.js
+++ js/header.js
@@ -4,6 +4,9 @@ const headerHTML = `
             <a class="NavLogo" href="/index" aria-label="Retour à l'accueil"><img src="/assets/icon.svg" alt="Logo de Lenny Gadroy" /></a>
             <ul class="NavLinks">
                 <li><a href="/index">Accueil</a></li>
+                <li><a href="/services">Services</a></li>
+                <li><a href="/reals">Réalisations</a></li>
+                <li><a href="/about">A propos</a></li>
             </ul>
             <div class="NavBtn"><a href="/contact" class="BtnSolid">Me contacter</a></div>
         </nav>
