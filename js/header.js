cb6142c0e71fb1be07c45a072eb17509356998e2
Lennouille <lennygadroy@gmail.com>
2026-05-22 11:28:29 +0200

    remove useless links

--- js/header.js
+++ js/header.js
@@ -4,16 +4,6 @@ const headerHTML = `
             <a class="NavLogo" href="/index" aria-label="Retour à l'accueil"><img src="/assets/icon.svg" alt="Logo de Lenny Gadroy" /></a>
             <ul class="NavLinks">
                 <li><a href="/index">Accueil</a></li>
-                <li class="Dropdown">
-                    <a href="#!">Réalisations <span class="Chevron">▼</span></a>
-                    <ul class="DropdownMenu">
-                        <li><a href="/projets">Projets</a></li>
-                        <li><a href="/designs">Designs</a></li>
-                        <li><a href="/albums">Albums</a></li>
-                    </ul>
-                </li>
-                <li><a href="/skills">Compétences</a></li>
-                <li><a href="/cv">Curriculum</a></li>
             </ul>
             <div class="NavBtn"><a href="/contact" class="BtnSolid">Me contacter</a></div>
         </nav>
