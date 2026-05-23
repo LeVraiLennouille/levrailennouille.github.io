4e5a8816befb7cff7dacc1dba88b5ee091d70aa5
Lennouille <lennygadroy@gmail.com>
2026-05-23 16:43:50 +0200

    upgrade readability

--- js/hero.js
+++ js/hero.js
@@ -9,14 +9,14 @@ const HeroHTML = `
                     <span class="T1A VT TS26">Wireframe</span>
                     <span class="T1B TS46">UX</span>
                     <span class="T1C VT TS30">Audits</span>
-                    <span class="T1D TS26">Mapping</span>
+                    <span class="T1D VT TS26">Mapping</span>
                     <span class="T1E VT TS30">Parcours</span>
                 </div>
                 <div class="SubText" id="SubT2">DESIGN
                     <span class="T2A VT TS26">Optimiser</span>
                     <span class="T2B TS46">UI</span>
                     <span class="T2C VT TS30">Layout</span>
-                    <span class="T2D TS26">Intégrer</span>
+                    <span class="T2D VT TS26">Intégrer</span>
                     <span class="T2E VT TS30">Affiner</span>
                 </div>
             </div>
