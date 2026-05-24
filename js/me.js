3eba8d9c1af4e9de197a2671bace98cf3f8ba388
Lennouille <lennygadroy@gmail.com>
2026-05-24 07:43:20 +0200

    upgrade UI

--- js/me.js
+++ js/me.js
@@ -18,10 +18,12 @@ const MeHTML = `
         </svg>
         <div class="Text">
             <h4>Qui se cache derrière Lennouille ?</h4>
-            <p>Salut ! Moi c'est Lenny, fondateur du studio Lennouille, ton partenaire créatif et technique.</p>
-            <p>Tout a commencé par une volonté de comprendre les mécaniques derrière les interfaces que nous utilisons chaque jour. De ma formation initiale centrée sur l'humain et la relation client à ma spécialisation web, cette curiosité s'est transformée en une véritable passion pour l'ergonomie, que j'ai forgée en grande partie en autodidacte.</p>
-            <p>Aujourd'hui, je ne me contente pas de dessiner des maquettes ou de taper du code : je m'implique, je conseille et je t'accompagne de A à Z. Mon objectif ? Révéler l'essence de ton projet et créer une interface qui te ressemble à 100 %.</p>
-            <p>Chaque projet est pensé de manière globale pour marquer les esprits et atteindre tes objectifs. Du Branding au Développement Web, en passant par l'UI/UX et la création de Design Systems pérennes, je conçois des produits fluides, performants et sans friction.</p>
+            <div class="Desc">
+                <p>Salut ! Moi c'est Lenny, fondateur du studio Lennouille, ton partenaire créatif et technique.</p>
+                <p>Tout a commencé par une volonté de comprendre les mécaniques derrière les interfaces que nous utilisons chaque jour. De ma formation initiale centrée sur l'humain et la relation client à ma spécialisation web, cette curiosité s'est transformée en une véritable passion pour l'ergonomie, que j'ai forgée en grande partie en autodidacte.</p>
+                <p>Aujourd'hui, je ne me contente pas de dessiner des maquettes ou de taper du code : je m'implique, je conseille et je t'accompagne de A à Z. Mon objectif ? Révéler l'essence de ton projet et créer une interface qui te ressemble à 100%.</p>
+                <p>Chaque projet est pensé de manière globale pour marquer les esprits et atteindre tes objectifs. Du Branding au Développement Web, en passant par l'UI/UX et la création de Design Systems pérennes, je conçois des produits fluides, performants et sans friction.</p>
+            </div>
             <a href="/about"><button class="GlowingBtn" type="button">HOVER ME, THEN CLICK ME!</button></a>
         </div>
     </section>
