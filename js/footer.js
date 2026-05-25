59e5d590e066c5a849191c3cde41ae97f6efb403
Lennouille <lennygadroy@gmail.com>
2026-05-25 16:46:30 +0200

    change linkedIn link

--- js/footer.js
+++ js/footer.js
@@ -32,7 +32,7 @@ const footerHTML = `
         <div class="FooterCol">
             <h4 class="FooterTitle">Contact</h4>
             <ul class="FooterLinks FooterSocialLinks">
-                <li><a href="https://linkedin.com/in/lennygadroy/" target="_blank" rel="noopener noreferrer">
+                <li><a href="https://linkedin.com/in/lennouille/" target="_blank" rel="noopener noreferrer">
                     <span class="FooterIcon">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"/><rect x="2" y="9" width="4" height="12"/><circle cx="4" cy="4" r="2"/></svg>
                     </span>LinkedIn
