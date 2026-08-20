e49aba59fdfd6cbeda38cac011e03b954fabdd38
Lennouille <lennygadroy@gmail.com>
2026-08-20 14:44:52 +0200

    export into JS multiblocks

---
+++ js/cta.js
@@ -0,0 +1,15 @@
+const CTAHTML = `
+    <section class="CTA">
+        <div class="CTAContent">
+            <span class="Badge">Disponible pour de nouveaux projets</span>
+            <h2>Un projet en tête ?</h2>
+            <p>Refonte, site vitrine ou simple avis extérieur sur votre interface : parlons-en. Vous pouvez aussi obtenir une estimation chiffrée en quelques clics.</p>
+            <div class="CTAButtons">
+                <a href="contact.html" class="ButtonSolid">Me contacter</a>
+                <a href="simu.html" class="ButtonGhost">Obtenir un devis</a>
+            </div>
+        </div>
+    </section>
+`;
+
+document.getElementById('CTAPlaceHolder').innerHTML = CTAHTML;
