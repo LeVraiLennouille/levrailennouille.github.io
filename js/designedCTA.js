254bf1b06a915b3b2694db97d597c6a99c9c6548
Lennouille <lennygadroy@gmail.com>
2026-08-23 15:47:48 +0200

    export into JS multiblocks

---
+++ js/designedCTA.js
@@ -0,0 +1,15 @@
+const DesignedCTAHTML = `
+    <section class="CTA">
+        <div class="Card">
+            <div class="Text">
+                <h2>Un site qui vous ressemble,<br><em>qui convertit vraiment.</em></h2>
+            </div>
+            <div class="Action">
+                <p>Premier échange offert. Réponse sous 24h. Devis clair avant tout engagement.</p>
+                <a href="contact.html" class="Btn">Discutons de votre projet <span class="Arrow">↗</span></a>
+            </div>
+        </div>
+    </section>
+`;
+
+document.getElementById('DesignedCTAPlaceHolder').innerHTML = DesignedCTAHTML;
