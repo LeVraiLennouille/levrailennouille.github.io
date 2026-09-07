211c91d136e3d78d166601d4558e7054da1e5a73
Lennouille <lennygadroy@gmail.com>
2026-09-07 14:55:09 +0200

    update terms links in footer

--- js/footer.js
+++ js/footer.js
@@ -59,9 +59,9 @@ const footerHTML = `
         <p>&copy; 2026 Lennouille. Tous droits réservés.</p>
         <div class="FooterTerms">
             <ul class="FooterLinks">
-                <li><a href="/mentions-legales">Mentions légales</a></li>
-                <li><a href="/politique-confidentialite">Politique de confidentialité</a></li>
-                <li><a href="/cgu">CGU</a></li>
+                <li><a href="/terms/mentions-legales">Mentions légales</a></li>
+                <li><a href="/terms/politique-confidentialite">Politique de confidentialité</a></li>
+                <li><a href="/terms/cgu">CGU</a></li>
             </ul>
             <button class="FooterBackTop" onclick="window.scrollTo({top:0,behavior:'smooth'})" aria-label="Retour en haut">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><polyline points="18 15 12 9 6 15"/></svg>
